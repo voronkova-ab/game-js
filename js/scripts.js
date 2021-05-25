b593f7667d7116e9892256d2a467b7a1e3e8b8a8
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-05-25 13:48:55 +0500

    Создала 4 объекта с вопросами

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-let hello = 'Привет';
-console.log(hello);
+const questionOne = {
+    question: 'Где обитает жираф?',
+    answer: 'В Индии',
+    currectAnswer: 'В Африке'
+};
+const questionTwo = {
+    question: 'Как быстро может бегать жираф?',
+    answer: '70 км/ч',
+    currectAnswer: '55 км/ч'
+};
+const questionThree = {
+    question: 'Сколько подвидов жирафа существует?',
+    answer: 5,
+    currectAnswer: 9
+};
+const questionFour = {
+    question: 'Сколько живет жираф?',
+    answer: '50 лет',
+    currectAnswer: '30 лет'
+};
